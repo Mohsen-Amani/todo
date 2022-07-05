2286a474cb287cd25e9147da26c4bc73a35434fa
Mohsen Amani <mohsenamani001@outlook.com>
2022-07-05 19:01:22 +0430

    create tasks, users, auth controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksController } from './tasks/tasks.controller';
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
+import { usersController } from './users/users.controller';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TasksModule, AuthModule, UsersModule],
+  controllers: [
+    AppController,
+    TasksController,
+    TasksController,
+    usersController,
+    AuthController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
